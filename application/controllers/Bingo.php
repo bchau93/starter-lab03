5516d27d920189433ea06065a8980c16a61c9b8f
bchau <bchau93@hotmail.com>
2015-09-26 17:31:05 -0700

    Added wisdom controller and routed comp4711/wisdom to bingo/wisdom

--- application/controllers/Bingo.php
+++ application/controllers/Bingo.php
@@ -14,9 +14,15 @@ class Bingo extends Application {
 
     function index() {
         $this->data['pagebody'] = 'justone';    // this is the view we want shown
-        $source = $this->quotes->get(4);
+        $source = $this->quotes->get(5);
         $this->data = array_merge($this->data, $source);
         $this->render();
     }
   
+    function wisdom(){
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        $source = $this->quotes->get(6);
+        $this->data = array_merge($this->data, $source);
+        $this->render();
+    }
 }
